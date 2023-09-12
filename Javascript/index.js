940bbc2e2882db4e6c73cd00bf3a594615d3f677
Deepak Rawat <144555924+deepak-rawat08@users.noreply.github.com>
2023-09-13 02:10:01 +0530

    Delete Javascript directory

--- Javascript/index.js
+++
@@ -1,8 +0,0 @@
-burger = document.querySelector(".burger");
-navbar = document.querySelector("#navbar");
-menu = document.querySelector("#menu");
-
-burger.addEventListener("click", () => {
-  menu.classList.toggle("v-class");
-  navbar.classList.toggle("h-nav");
-});
